d8baf46a3b86e79ae9ae56b358f6045a8b16d8a5
rgopisettiaws12 <140700511+rgopisettiaws12@users.noreply.github.com>
2024-02-23 15:32:43 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,6 @@ public class App
         RedisJava cache = new RedisJava();
         cache.cache_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything",String.valueOf(ans));
 
-        System.out.println("Cached value: " + cache.get_chached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
+        System.out.println("Cached value: " + cache.get_cached_value("answer_to_the_ultimate_question_of_life_the_universe_and_everything"));
     }
 }
